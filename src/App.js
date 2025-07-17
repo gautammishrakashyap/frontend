fd3b46fad28cf50653688866dda6989bcb644c39
Kumar Gautam <kumar@humantics.com>
2025-07-17 17:39:08 +0530

    file Setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+const image = require('./assest/loog.png'); // Adjust the path if needed
 
 function App() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    fetch('http://localhost:5000/api/message')
+      .then(res => res.json())
+      .then(data => setMessage(data.message));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='header'>
+        <img src={image} alt="Library Logo" className='logo' />
+        <a href="#" className='link'>Library</a>
+        <a href="#" className='link'>Home</a>
+        <a href="#" className='link'>About</a>
+        <a href="#" className='link'>Contact</a>
+      </div>
+
+      <div className='container'>
+        
+          <h1 className="background-text">Library </h1>
+          <h1 className="foreground-text">Manish</h1>
+       
+
+        
+        <p>{message}</p>
+      </div>
+      {/* Remove the uper-container or use it for something else if you want */}
+    </>
   );
 }
 
