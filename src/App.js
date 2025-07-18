53cd856adc80ae09491d18bb8d5a8e2c592ebbd7
Kumar Gautam <kumar@humantics.com>
2025-07-18 16:37:25 +0530

    change

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
-const image = require('./assest/loog.png'); // Adjust the path if needed
+const image = require('./assest/loog.png');
+const img = require('./assest/book.png'); // Adjust the path if needed
+const moon = require('./assest/Moon.png'); // Adjust the path if needed
 
 function App() {
   const [message, setMessage] = useState('');
@@ -24,13 +26,26 @@ function App() {
       <div className='container'>
         
           <h1 className="background-text">Library </h1>
-          <h1 className="foreground-text">Manish</h1>
+          
        
 
         
-        <p>{message}</p>
+        {/* <p>{message}</p> */}
       </div>
       {/* Remove the uper-container or use it for something else if you want */}
+
+      <div className='moon'>
+
+        <img src={moon} alt="MOON H " className='cha' />
+      </div>
+
+      <div className='second-page'>
+        <h1 className='second-page-text'></h1>
+        <p className='sec-tital'>A library is a collection of books, and possibly other materials and media, that is accessible for use by its members and members of allied institutions. Libraries provide physical (hard copies) or digital (soft copies) materials, and may be a physical location, a virtual space, or both.
+        </p>
+        <img src={img} alt="Library Logo" className='logo' />
+      </div>
+          
     </>
   );
 }
